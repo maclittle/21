104168c94221d668bb80ddd50b3c9d8062b6dabe
maclittle <maclittle@Mackenzies-MacBook-Pro.local>
2016-11-22 11:43:56 -0500

    Added in Second Agent
    The agent will make its own predictions. If they agree, nothing
    changes. If they disagree, the participant’s trust level affects
    whether or not they will change their answer to reflect the second
    agent’s. If the result is an improvement, trust increases, and if it
    pushes over 21, trust decreases

--- 21 Model.R
+++ 21 Model.R
@@ -4,8 +4,9 @@ cards.left <- whole.deck
 
 risk <- 0.5
 risk.list <- c()
-trust <- 0.5
+trust <- 0.1
 trust.list <- c()
+agree.count <- 0
 hand <- 0
 total.score <- 0
 stop <- FALSE
@@ -30,19 +31,59 @@ draw <- function() {
 # Plays a single round of 21
 play.round <- function() {
   initial.hand()
+  advice.taken <- FALSE
   # While hand is not over 21 and you haven't stopped
   while((hand<21) && (stop==FALSE)) {
-    # If hand is less than 11 draw another card
+    # If hand is less than 11 draw another card, unless...
     if (hand<11){
-      draw()
+      # If the other agent tells you to stop even though it's safe, and you agree, stop
+      bad.advice <- rbinom(1, 1, 0.1)
+      agree <- rbinom(1, 1, trust)
+      if((bad.advice== 1)&&(agree==1)){
+        stop <<- TRUE
+        agree.count <<- agree.count + 1
+        advice.taken <- TRUE
+      }
+      # Otherwise draw another card
+      if((bad.advice==0) || (agree==0)){
+        draw()
+      }
     }
     # Generate a number based on the risk factor. 1 means draw, 0 means stop
     risk.huh <- rbinom(1, 1, risk)
-    if (risk.huh == 1){
+    # Predicts whether it's safe to draw or not
+    good.advice <- rbinom(1, 1, 0.3)
+    # If you agree to draw, draw
+    if ((risk.huh == 1)&&(good.advice==1)){
       draw()
     }
-    if (risk.huh == 0){
+    # If you agree to stop, stop
+    if ((risk.huh == 0)&&(good.advice==0)){
       stop <<- TRUE
+    }
+    # If you want to draw and it says stop:
+    if((risk.huh==1)&&(good.advice==0)){
+      agree <- rbinom(1, 1, trust)
+      if(agree==1){
+        stop <<- TRUE
+        agree.count <<- agree.count + 1
+        advice.taken <- TRUE
+      }
+      if(agree==0){
+        draw()
+      }
+    }
+    # If you want to stop and it says draw:
+    if((risk.huh==0)&&(good.advice==1)){
+      agree <- rbinom(1, 1, trust)
+      if(agree==1){
+        draw()
+        agree.count <<- agree.count + 1
+        advice.taken <- TRUE
+      }
+      if(agree==0){
+        stop <<- TRUE
+      }
     }
   }
   # If you stop and hand is below 22, add hand score to total score
@@ -54,6 +95,12 @@ play.round <- function() {
     if(risk>1){
       risk <<- 1
     }
+    if(advice.taken==TRUE){
+      trust <<- trust + rnorm(1, mean=.03, sd = 0.005)
+      if(trust>1){
+        trust <<- 1
+      }
+    }
   }
   # If you exceed 21, no points are added to total score, and fail count increases
   if(hand>21){
@@ -63,13 +110,21 @@ play.round <- function() {
     if(risk<0){
       risk <<- 0
     }
+    if(advice.taken==TRUE){
+      trust <<- trust - rnorm(1, mean=.1, sd = 0.05)
+      if(trust<0){
+        trust <<- 0
+      }
+    }
   }
   # Reset settings to a new round
   hand <<- 0
   cards.left <<- whole.deck
   stop <<- FALSE
+  advice.taken <- FALSE
   # Keep track of the risk over time
   risk.list <<- c(risk.list, risk)
+  trust.list <<- c(trust.list, trust)
 }
 
 # Play a bunch of rounds and return risk, fails, final score, and avg stop value from stop list
@@ -91,3 +146,5 @@ run.bunch <- function(n){
 run.bunch(100)
 # See that person's risk factor over the 100 rounds
 plot(1:100, risk.list, type = 'l')
+# Trust factor:
+plot(1:100, trust.list, type = 'l')
