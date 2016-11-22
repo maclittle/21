d8c657cb5bf6e030a15e83f53be776ab4369adfa
maclittle <maclittle@Mackenzies-MacBook-Pro.local>
2016-11-22 11:18:09 -0500

    Added more comments

--- 21 Model.R
+++ 21 Model.R
@@ -1,3 +1,4 @@
+# Initialize the whole deck of cards
 whole.deck <- c(2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10)
 cards.left <- whole.deck
 
@@ -11,6 +12,7 @@ stop <- FALSE
 fails <- 0
 stop.list <- c()
 
+# Draws the initial hand of 2 cards
 initial.hand <- function(){
   start.ids <- sample(1:36, 2, replace= FALSE)
   start.hand <- cards.left[start.ids]
@@ -18,20 +20,23 @@ initial.hand <- function(){
   hand <<- sum(start.hand)
 }
 
+# Draws one card from the deck and adds it to your hand
 draw <- function() {
   next.id <- sample(1:length(cards.left), 1, replace= FALSE)
   hand <<- hand + cards.left[next.id]
   cards.left <<- cards.left[-next.id]
 }
 
+# Plays a single round of 21
 play.round <- function() {
   initial.hand()
+  # While hand is not over 21 and you haven't stopped
   while((hand<21) && (stop==FALSE)) {
-    #if hand is less than 11 draw another card
+    # If hand is less than 11 draw another card
     if (hand<11){
       draw()
     }
-    #gen num from risk factor. if 1, draw. if not, stop==TRUE.
+    # Generate a number based on the risk factor. 1 means draw, 0 means stop
     risk.huh <- rbinom(1, 1, risk)
     if (risk.huh == 1){
       draw()
@@ -40,28 +45,34 @@ play.round <- function() {
       stop <<- TRUE
     }
   }
+  # If you stop and hand is below 22, add hand score to total score
   if((stop == TRUE)&& (hand<22)){
     total.score <<- total.score + hand
     stop.list <<- c(stop.list, hand)
+    # The risk factor gets a little higher
     risk <<- risk + rnorm(1, mean=.03, sd = 0.005)
     if(risk>1){
       risk <<- 1
     }
   }
+  # If you exceed 21, no points are added to total score, and fail count increases
   if(hand>21){
     fails <<- fails + 1
+    # Risk factor decreases
     risk <<- risk - rnorm(1, mean=.1, sd = 0.05)
     if(risk<0){
       risk <<- 0
     }
   }
+  # Reset settings to a new round
   hand <<- 0
   cards.left <<- whole.deck
   stop <<- FALSE
+  # Keep track of the risk over time
   risk.list <<- c(risk.list, risk)
 }
 
-#play a bunch of rounds and return final score, fails, and avg stop value from stop list
+# Play a bunch of rounds and return risk, fails, final score, and avg stop value from stop list
 run.bunch <- function(n){
   for (i in 1:n){
     play.round()
@@ -76,5 +87,7 @@ run.bunch <- function(n){
   print(mean(stop.list))
 }
 
+# Run 100 rounds with 1 person
 run.bunch(100)
-plot(1:100, risk.list)
+# See that person's risk factor over the 100 rounds
+plot(1:100, risk.list, type = 'l')
