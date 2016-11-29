180eb2b7d93688ebe45a4f389617fd8824fafcf7
maclittle <maclittle@Mackenzies-MacBook-Pro.local>
2016-11-29 11:38:45 -0500

    Exp conditions and recovery start
    Generated sample data for multiple participants. Played around with
    optim and DEoptim to attempt model recovery, but there are errors to be
    worked out. Added experience conditions that affect trust recovery.

--- 21 Model.R
+++ 21 Model.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+library(DEoptim)
+
 # Initialize the whole deck of cards
 whole.deck <- c(2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10)
 cards.left <- whole.deck
@@ -12,6 +15,8 @@ total.score <- 0
 stop <- FALSE
 fails <- 0
 stop.list <- c()
+experience.trust <- 0.03
+experience.level <- 0
 
 # Draws the initial hand of 2 cards
 initial.hand <- function(){
@@ -38,6 +43,12 @@ play.round <- function() {
     if (hand<11){
       # If the other agent tells you to stop even though it's safe, and you agree, stop
       bad.advice <- rbinom(1, 1, 0.1)
+      if(trust>1){
+        trust <<- 1
+      }
+      if(trust<0){
+        trust <<- 0
+      }
       agree <- rbinom(1, 1, trust)
       if((bad.advice== 1)&&(agree==1)){
         stop <<- TRUE
@@ -50,6 +61,12 @@ play.round <- function() {
       }
     }
     # Generate a number based on the risk factor. 1 means draw, 0 means stop
+    if(risk>1){
+      risk <<- 1
+    }
+    if(risk<0){
+      risk <<- 0
+    }
     risk.huh <- rbinom(1, 1, risk)
     # Predicts whether it's safe to draw or not
     good.advice <- rbinom(1, 1, 0.3)
@@ -63,6 +80,12 @@ play.round <- function() {
     }
     # If you want to draw and it says stop:
     if((risk.huh==1)&&(good.advice==0)){
+      if(trust>1){
+        trust <<- 1
+      }
+      if(trust<0){
+        trust <<- 0
+      }
       agree <- rbinom(1, 1, trust)
       if(agree==1){
         stop <<- TRUE
@@ -75,6 +98,12 @@ play.round <- function() {
     }
     # If you want to stop and it says draw:
     if((risk.huh==0)&&(good.advice==1)){
+      if(trust>1){
+        trust <<- 1
+      }
+      if(trust<0){
+        trust <<- 0
+      }
       agree <- rbinom(1, 1, trust)
       if(agree==1){
         draw()
@@ -96,7 +125,7 @@ play.round <- function() {
       risk <<- 1
     }
     if(advice.taken==TRUE){
-      trust <<- trust + rnorm(1, mean=.03, sd = 0.005)
+      trust <<- trust + rnorm(1, mean=experience.trust, sd = 0.005)
       if(trust>1){
         trust <<- 1
       }
@@ -127,24 +156,80 @@ play.round <- function() {
   trust.list <<- c(trust.list, trust)
 }
 
+experience.levels <- c()
+risks <- c()
+trusts <- c()
+total.scores <- c()
+
 # Play a bunch of rounds and return risk, fails, final score, and avg stop value from stop list
 run.bunch <- function(n){
+  # Determines whether participant is in high or low exerience condition
+  experience.level <<- rbinom(1, 1, 0.5)
+  # If they think the second agent has high experience, they trust the successes more and start with higher trust
+  if(experience.level==1){
+    experience.trust <<- 0.06
+    trust <<- 0.3
+  }
+  # If they think the second agent has low experience, they trust the successes less and start with lower trust
+  if(experience.level==0){
+    experience.trust <<- 0.03
+    trust <<- 0.1
+  }
   for (i in 1:n){
     play.round()
   }
-  print("risk:")
-  print(risk)
-  print("number of fails: ")
-  print(fails)
-  print("total score:")
-  print(total.score)
-  print("average stopping point of success:")
-  print(mean(stop.list))
+  #print("experience level: (1 is high, 0 is low)")
+  #print(experience.level)
+  #print("risk:")
+  #print(risk)
+  #print("number of fails: ")
+  #print(fails)
+  #print("total score:")
+  #print(total.score)
+  #print("average stopping point of success:")
+  #print(mean(stop.list))
+  
+  experience.levels <<- c(experience.levels, experience.level)
+  risks <<- c(risks, risk)
+  trusts <<- c(trusts, trust)
+  total.scores <<- c(total.scores, total.score)
 }
 
-# Run 100 rounds with 1 person
-run.bunch(100)
-# See that person's risk factor over the 100 rounds
-plot(1:100, risk.list, type = 'l')
-# Trust factor:
-plot(1:100, trust.list, type = 'l')
+# Run 300 rounds with 1 person
+run.bunch(300)
+experience.levels <<- c()
+risks <<- c()
+trusts <<- c()
+total.scores <<- c()
+scores.over.time <<- data.frame(risk.list, trust.list)
+# See that person's risk factor over the 300 rounds with trust as a color
+ggplot(scores.over.time, aes(x=1:300, y=risk.list, color=trust.list))+
+  geom_line()
+# Trust factor with risk as a color:
+ggplot(scores.over.time, aes(x=1:300, y=trust.list, color=risk.list))+
+  geom_line()
+
+
+# generate data for a bunch of participants after 100 trials each (final risk, final trust, final score, category)
+replicate(100, {run.bunch(50)})
+# Generated data
+gen.data <<- data.frame(experience.levels, risks, trusts, total.scores)
+
+# see whether the model can predict which category the data comes from
+
+rmse.from.models <- function(params) {
+  risk <<- params[1]
+  trust <<- params[2]
+  new.data <- replicate(1000, {run.bunch(50)})
+  
+  rmse.squares <- (new.data-gen.data)^2
+  rmse <- sqrt(mean(rmse.squares))
+  return(rmse)
+}
+
+pars <- c(0.5,0.5)
+optim.result <- optim(pars, rmse.from.models, method="Nelder-Mead")
+
+deoptim.result <- DEoptim(rmse.from.models, c(0,0), c(1,1))
+deoptim.result$par
+deoptim.result$val
